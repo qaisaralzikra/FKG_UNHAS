3161d7cabaad55fdd31ef5abbbcd0b33e66275f4
FKG Dev <dev@fkg-unhas.local>
2026-05-29 13:05:27 +0800

    api local

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // HAPUS tanda / di ujung kata api
-    baseURL: 'https://fkg-unhas.qaisaralzikrah.workers.dev',
+    // baseURL: 'https://fkg-unhas.qaisaralzikrah.workers.dev',
+    baseURL: 'http://localhost:8080/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
